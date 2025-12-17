1e1e8f0c8a2dd63023f7ab82749e495897ac7b13
Higor16021995 <higorletras@hotmail.com>
2025-12-17 18:35:03 -0300

    Update script.js

--- script.js
+++ script.js
@@ -23,3 +23,18 @@ async function processar() {
     document.getElementById("status").innerHTML = "Erro ao processar.";
   }
 }
+function mostrar(data) {
+  let html = "<table><tr><th>Descrição</th><th>Vend.</th><th>Estoque</th><th>Mín.</th><th>Comprar</th></tr>";
+  data.forEach(x => {
+    html += `<tr>
+      <td>${x.Descricao}</td>
+      <td>${x.Qtd}</td>
+      <td>${x.Estoque}</td>
+      <td>${x.EstoqueMin}</td>
+      <td>${x.QtdRecomendada}</td>
+    </tr>`;
+  });
+  html += "</table>";
+  document.getElementById("status").innerHTML = "";
+  document.getElementById("result").innerHTML = html;
+}
